e2e673c272bc664f4f3700180573c52766f1db96
Ons Bouneb <onsbouneb1@gmail.com>
2019-08-21 00:20:00 +0200

    BasicScala done

--- app/BasicScala.scala
+++ app/BasicScala.scala
@@ -18,12 +18,33 @@ object BasicScala {
     * output : ""
     */
   def encodeParamsInUrl(params: Map[String, String]): String = ???
+def encodeParamsInUrl(params: Map[String, String]): String = {
 
+    if (params.isEmpty) {
+        return("")
+    }
+    else{
+    
+  var result = "?"
+     for ((k,v) <- params) {
+    result = result + (s"$k=$v&")
+}
+
+    val newResult = new StringBuilder(result).deleteCharAt(result.length()-1).toString();
+
+  return(newResult)
+}
+
+   
+
+}
 
   /**
     * Test if a String is an email
     */
-  def isEmail(maybeEmail: String): Boolean = ???
+  def isEmail(maybeEmail: String): Boolean = 
+
+	maybeEmail.contains("@")
 
 
   /**
@@ -37,7 +58,8 @@ object BasicScala {
     * input : (i = 99, n = 38997)
     * output : 1723793299
     */
-  def power(i:Int, n:Int):Int = ???
+  def power(i:Int, n:Int):Int = 
+		scala.math.pow(i,n).toInt 
 
 
 }
